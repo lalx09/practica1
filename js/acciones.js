dda4db30109c075455ac24ec6f03e4ccdbe6fbf8
lalx09 <brycekc_10@hotmail.com>
2014-10-12 14:16:17 -0500

    Practica1
    imagenes

---
+++ js/acciones.js
@@ -0,0 +1,17 @@
+//JavaScript
+	
+$(document).ready (function(){
+	$('#B1').click(function(){
+		$('#img1').show();
+	});//click
+});//ready
+$(document).ready (function(){
+	$('#B2').click(function(){
+		$('#img2').show();
+	});//click
+});//ready
+$(document).ready (function(){
+	$('#B3').click(function(){
+		$('#img3').show();
+	});//click
+});//ready
